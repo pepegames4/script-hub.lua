c3be9fcc897677a6010c7a01c0f89c3011abe1e8
pepegames4 <137959037+pepegames4@users.noreply.github.com>
2025-08-20 16:01:51 +0200

    script-hub.lua

--- script-hub.lua
+++ script-hub.lua
@@ -4,7 +4,7 @@ local gameId = game.PlaceId
 -- URLs de los scripts para cada juego.
 local scripts = {
     [6097258548] = "https://raw.githubusercontent.com/pepegames4/Iron-man-2-script.lua/main/main.lua", -- Iron Man 2 Simulator
-    [109852230] = "https://raw.githubusercontent.com/pepegames4/Kat-x-script.lua/main/main.lua", -- ID de Katana Simulator (ejemplo)
+    [111163066268338] = "https://raw.githubusercontent.com/pepegames4/Kat-x-script.lua/main/main.lua",
 }
 
 local scriptUrl = scripts[gameId]
