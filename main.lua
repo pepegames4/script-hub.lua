25b7deef5ad40fa3ae2b4b10b7cfb846e627ede0
pepegames4 <137959037+pepegames4@users.noreply.github.com>
2025-08-29 15:22:01 +0200

    main.lua

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ local gameId = game.PlaceId
 local scripts = {
     [6097258548] = "https://raw.githubusercontent.com/pepegames4/Iron-man-2-script.lua/main/main.lua",
     [111163066268338] = "https://raw.githubusercontent.com/pepegames4/Kat-x-script.lua/main/main.lua",
+    [108947879689560] = "https://raw.githubusercontent.com/pepegames4/Iron-man-2-script.lua/refs/heads/main/main.lua",
 }
 
 local scriptUrl = scripts[gameId]
